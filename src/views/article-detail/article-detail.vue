19b9ba0e1f3701e6d05cc69f748577c38f872f4a
Duy Nguyen <duy.nguyencong@pycogroup.com>
2018-08-20 01:20:51 +0700

    feat(app): adds delete function

--- src/views/article-detail/article-detail.vue
+++ src/views/article-detail/article-detail.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="container">
-    <ArticleItem
-      id="article-item"
-      v-if="!!article"
-      v-bind="article"
-    />
+    <div v-if="!!article">
+      <ArticleItem
+        id="article-item"
+        v-bind="article"
+      />
+      <hr />
+      <div id="footer">
+        <Button @click="handleEditClick">Edit</Button>
+        <Button @click="handleDeleteClick">Delete</Button>
+      </div>
+    </div>
     <h3 v-else>Item not found</h3>
   </div>
 </template>
@@ -12,9 +18,10 @@
 <script>
 import Storage from "@/services/storage/storage";
 import ArticleItem from "@/components/article-item/article-item";
+import Button from "@/elements/button/button";
 
 export default {
-  components: { ArticleItem },
+  components: { ArticleItem, Button },
   data() {
     return {
       id: "",
@@ -28,8 +35,16 @@ export default {
   },
   methods: {
     getArticle() {
-      this.id = this.$router.currentRoute.params.id;
+      this.id = parseInt(this.$router.currentRoute.params.id);
       this.article = Storage.getById(parseInt(this.id, 10));
+    },
+    handleEditClick() {},
+    handleDeleteClick() {
+      const confirmation = confirm("Are you sure?");
+      if (confirmation === true) {
+        Storage.remove(this.id);
+        this.$router.push("/articles");
+      }
     }
   },
   created() {
