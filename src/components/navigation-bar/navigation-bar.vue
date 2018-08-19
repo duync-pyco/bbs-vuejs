b1da68e1ce454485164d269b7833fcd78b45d7ee
Duy Nguyen <duy.nguyencong@pycogroup.com>
2018-08-20 02:05:40 +0700

    feat(navigation): adds buttons

--- src/components/navigation-bar/navigation-bar.vue
+++ src/components/navigation-bar/navigation-bar.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>{{ this.title }}</h1>
+  <div id="container">
+    <router-link to="/articles">
+      <Button class="navigation-button">Article List</Button>
+    </router-link>
+    <h1 id="title">{{ this.title }}</h1>
+    <router-link to="/new-article">
+      <Button class="navigation-button">New Article</Button>
+    </router-link>
+  </div>
 </template>
 
 <script>
+import Button from "@/elements/button/button";
+
 export default {
+  components: { Button },
   props: {
     title: {
       type: String,
