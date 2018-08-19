be1a2729fbaa5f3e139abac26fb1bd18c938d9ca
Duy Nguyen <duy.nguyencong@pycogroup.com>
2018-08-19 23:18:20 +0700

    feat(articles): adds pageSize

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ export default new Router({
       component: NewArticle
     },
     {
-      path: "/articles",
+      path: "/articles/:pageSize?",
       name: "Articles",
       component: Articles
     },
--- src/views/articles/articles.vue
+++ src/views/articles/articles.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="container">
     <ul>
-      <li id="article" v-for="article in sortedArticles" :key="article.id">
+      <li id="article" v-for="article in processedArticles" :key="article.id">
         <ArticleItem v-bind="article"/>
       </li>
     </ul>
+    <hr>
+    <div id="select-container">
+      <span id="select-label">Articles per page</span>
+      <select name="Articles per page" v-model="pageSize">
+        <option
+          v-for="(pageSizeOption, index) in pageSizeOptions"
+          :key="index"
+          :value=pageSizeOption
+        >
+          {{ pageSizeOption }} items
+        </option>
+      </select>
+    </div>
   </div>
 </template>
 
@@ -15,17 +28,34 @@ import ArticleItem from "@/components/article-item/article-item";
 export default {
   data() {
     return {
-      articles: []
+      articles: [],
+      pageSizeOptions: [1, 2, 3, 5, 8],
+      pageSize: 2,
+      pageIndex: 1
     };
   },
   computed: {
-    sortedArticles() {
-      return this.articles.sort((a, b) => a.id < b.id);
+    start() {
+      return (this.pageIndex - 1) * this.pageSize;
+    },
+    end() {
+      return this.start + this.pageSize;
+    },
+    processedArticles() {
+      return this.articles
+        .sort((a, b) => a.id < b.id)
+        .slice(this.start, this.end);
+    }
+  },
+  watch: {
+    pageSize() {
+      this.$router.replace({ params: { pageSize: this.pageSize } });
     }
   },
   components: { ArticleItem },
   created() {
     this.articles = Storage.getAll();
+    this.$router.replace({ params: { pageSize: this.pageSize } });
   }
 };
 </script>
